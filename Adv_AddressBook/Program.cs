749a95e8c85a888e022d5730d67342c98fc08bf3
Tushar Anand <tusharranand@gmail.com>
2022-04-24 17:23:12 +0530

    [Tushar Anand] Added: Address Book Database

--- Adv_AddressBook/Program.cs
+++ Adv_AddressBook/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Adv_AddressBook
 {
     public class Program
     {
+        static string ConnectionStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Addressbook;Integrated Security=SSPI";
+        SqlConnection connection = new SqlConnection(ConnectionStr);
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Advance Addressbook Program");
--- SQL_AddressBook.sql
+++ SQL_AddressBook.sql
@@ -1,1 +1,2 @@
 --Welcome to Advance Addressbook Program
+create database Addressbook
